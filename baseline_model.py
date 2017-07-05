9ef9f8b8eef968f6ddfb6a55c0c19d8238b47329
Eddie Pease <eddiepease1@gmail.com>
2017-07-05 15:58:50 +0100

    first fully operational baseline model

--- baseline_model.py
+++ baseline_model.py
@@ -2,10 +2,12 @@ import os
 import time
 import pickle
 import numpy as np
+import pandas as pd
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.feature_extraction import DictVectorizer
+from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 from collections import defaultdict,Counter
 
@@ -20,9 +22,7 @@ class BaselineModel():
         self.train = train
         self.test = test
         self.cv_job_normalizer = CVJobNormalizer()
-        # self.train_features = create
-        # self.eval_metric = Evaluation_Metric()
-        # read this embedding in
+
 
     def prepare_feature_generation(self):
         skills_profile_dict = read_ontology_data('skill-profiles',file_type='pkl')
@@ -41,6 +41,7 @@ class BaselineModel():
 
     # create the features + labels for ML
     def create_bag_of_skills_features(self, df, tf_idf = True, job_to_predict=0):
+        print('preparing bag of skills features...')
 
         # read in pre-requisites
         skill_profile_dict, unique_skills, job_dict, reverse_job_dict = self.prepare_feature_generation()
@@ -48,7 +49,7 @@ class BaselineModel():
         # create dict
         bos_dict = dict.fromkeys(unique_skills, 0)
 
-        v = DictVectorizer(sparse=True)
+        v = DictVectorizer(sparse=True) # TODO:save as numpy array rather than sparse matrix
         features = []
         labels = []
 
@@ -63,7 +64,6 @@ class BaselineModel():
                     (normalized_title_feat in skill_profile_dict and normalized_title_label in skill_profile_dict):
 
                 temp_dict = bos_dict
-                normalized_title_feat = self.cv_job_normalizer.normalized_job(df, n_row=i, job_num=job_to_predict + 1)
                 # print(df['employment_history'][i])
                 # print(normalized_title_feat)
                 skills = skill_profile_dict[normalized_title_feat][0]
@@ -83,18 +83,35 @@ class BaselineModel():
 
         return X,labels
 
-    # TODO: complete this without duplication of code
-    def create_embedding_features(self,embeddings_filepath):
-        # run the embedding
-        embedding, ordered_job_title = create_job_embedding(embedding_size=100)
+    # create features and labels using embeddings
+    def create_embedding_features(self, df, tf_idf=False, job_to_predict=0):
+        print('preparing embedding features...')
 
+        # read in pre-requisites
+        _, _, job_dict, reverse_job_dict = self.prepare_feature_generation()
 
-        # loop through the CVs, (cutting out jobs as previously)
+        features_dict = {}
+        labels = []
 
-        # find the index of normalized job title
+        # read most recent job(s) from CV
+        # for i in range(0,1000):
+        for i in range(0, len(df)):
+            normalized_title_feat = self.cv_job_normalizer.normalized_job(df, n_row=i, job_num=job_to_predict + 1)
+            normalized_title_label = self.cv_job_normalizer.normalized_job(df, n_row=i, job_num=job_to_predict)
 
-        # extract embedding at this index and incrementally build features + labels
-        pass
+            # if no job_title in CV or job title is not in skills profile for either position
+            if (normalized_title_feat is not None and normalized_title_label is not None) and \
+                    (normalized_title_feat in self.ordered_job_title and normalized_title_label in self.ordered_job_title):
+
+                job_idx = self.ordered_job_title.index(normalized_title_feat)
+                features_dict[i] = self.embedding[job_idx,:]
+                labels.append(job_dict[normalized_title_label])
+
+        # convert dict into numpy array
+        features = pd.DataFrame(features_dict)
+        X = np.transpose(np.array(features))
+
+        return X, labels
 
     # plot a histogram of distribution of frequency of job titles
     def plot_job_frequency(self, labels, save_name):
@@ -103,14 +120,18 @@ class BaselineModel():
         plt.hist(values,bins=100)
         plt.savefig(save_name)
 
+    # return a list of values that appear infrequently
+    def return_infrequent_jobs(self, y_1, y_2, threshold):
+        y = y_1 + y_2
+        c = Counter(y)
+        low_count_values = [key for key, value in c.items() if value < threshold]
+        return low_count_values
 
     # method to remove jobs which don't appear often
-    def remove_infrequent_jobs(self,features, labels, threshold):
+    def remove_infrequent_jobs(self,features, labels, low_count_jobs):
 
         # return indices at which jobs appear that are below the threshold
-        c = Counter(labels)
-        low_count_values = [key for key,value in c.items() if value < threshold]
-        low_count_indices = [i for i,idx in enumerate(labels) if idx in low_count_values]
+        low_count_indices = [i for i,idx in enumerate(labels) if idx in low_count_jobs]
         remaining_indices = list(set(list(range(0,len(labels)))) - set(low_count_indices))
 
         # return the features and labels without these indices
@@ -120,11 +141,17 @@ class BaselineModel():
         return transformed_features, transformed_labels
 
     # function to save data so no need to re-run expensive process
-    def save_transformed_data(self, weighted, save_name):
+    def save_transformed_data(self, embedding, weighted, save_name):
+        print('saving transformed features...')
 
         #generate
-        X_train, y_train = self.create_bag_of_skills_features(self.train, tf_idf=weighted)
-        X_test, y_test = self.create_bag_of_skills_features(self.test, tf_idf=weighted)
+        if embedding:
+            self.embedding, self.ordered_job_title = create_job_embedding(embedding_size=100)
+            X_train, y_train = self.create_embedding_features(self.train)
+            X_test, y_test = self.create_embedding_features(self.test)
+        else:
+            X_train, y_train = self.create_bag_of_skills_features(self.train, tf_idf=weighted)
+            X_test, y_test = self.create_bag_of_skills_features(self.test, tf_idf=weighted)
 
         # save
         path = 'data/' + save_name + '/'
@@ -140,33 +167,64 @@ class BaselineModel():
         self.plot_job_frequency(y_train, save_name=path+'y_train_histogram.png')
 
     # load features and labels, either from saved or generate from
-    def load_transformed_data(self,save_name):
+    def load_transformed_data(self,save_name,remove_rare):
 
+        # load data
         path = 'data/' + save_name + '/'
         self.X_train = pickle.load(open(path + "X_train.pkl", "rb"))
         self.y_train = pickle.load(open(path + "y_train.pkl", "rb"))
         self.X_test = pickle.load(open(path + "X_test.pkl", "rb"))
         self.y_test = pickle.load(open(path + "y_test.pkl", "rb"))
 
+        print('Num train classes: ', len(set(self.y_train)))
+        print('Num test classes: ', len(set(self.y_test)))
 
-    def train_svm(self):
-        # svm = SVC()
+        # load data
+        if remove_rare == True:
+            threshold = 5
+            low_freq_jobs = self.return_infrequent_jobs(self.y_train,self.y_test,threshold)
+            self.X_train, self.y_train = self.remove_infrequent_jobs(self.X_train,self.y_train,
+                                                                     low_count_jobs=low_freq_jobs)
+            self.X_test, self.y_test = self.remove_infrequent_jobs(self.X_test, self.y_test,
+                                                                   low_count_jobs=low_freq_jobs)
+            print('Num train classes after remove: ', len(set(self.y_train)))
+            print('Num test classes after remove: ', len(set(self.y_test)))
 
-        pass
+        print('X_train shape: ', self.X_train.shape)
+        print('X_test shape: ', self.X_test.shape)
 
-    def train_naive_bayes(self):
-        pass
+    # function to evaluate performance
+    def mpr_scorer(self, clf, X, y):
+        class_labels = clf.classes_
+        n_class = float(len(class_labels))
+        y_pred_proba = clf.predict_proba(X)
+        # discard test samples for which the class was not seen in the training set
+        # (typically happens when train_size <~ number of classes)
+        mpr = np.mean([np.where(class_labels[y_pred_proba[i].argsort()[::-1]] == y[i])[0][0] / n_class
+                       for i in range(len(y))
+                       if y[i] in class_labels])
+        return mpr
 
-    def evaluate_model(self):
-        pass
+    # TODO: test this
+    def train_and_eval_model(self, save_name):
 
+        # load data into object
+        self.load_transformed_data(save_name,remove_rare=True)
 
+        # train model
+        print('Training model...')
+        t0 = time.time()
+        gnb = GaussianNB()
+        gnb.fit(self.X_train,self.y_train)
+        t1 = time.time()
+        print('Training time', t1-t0)
 
-    def define_and_train_model(self):
+        # evaluate model
+        print('Evaluating model...')
+        mpr = self.mpr_scorer(clf=gnb,X=self.X_test,y=self.y_test)
 
-        # this needs to include some reference to the evaluation metric
+        return mpr
 
-        pass
 
 if __name__ == "__main__":
 
@@ -176,32 +234,9 @@ if __name__ == "__main__":
 
 
     # form features
-    folder_name = 'processed_sample_unweighted_bos'
+    folder_name = 'processed_sample5_unweighted_embed'
     model = BaselineModel(train,test)
-    model.save_transformed_data(weighted=False,save_name=folder_name)
-    model.load_transformed_data(save_name=folder_name)
-
-    print(model.X_train.shape)
-    print(len(model.y_train))
-    print(model.X_test.shape)
-    print(len(model.y_test))
-
-
-
-
-    # X_trans, y_trans = model.remove_infrequent_jobs(X,y,threshold=2)
-    # print(X_trans.shape)
-    # print(len(y_trans))
-
-
-
-    # model.create_train_test()
-    # X_train = model.X_train
-    # y_train = model.y_train
-    #
-    # t1 = time.time()
-    #
-    # print('Duration is', t1-t0)
-    #
-    # print(X_train.shape)
-    # print(len(y_train))
+    # model.save_transformed_data(embedding=True, weighted=False,save_name=folder_name)
+    # model.load_transformed_data(save_name=folder_name,remove_rare=True)
+    mpr = model.train_and_eval_model(folder_name)
+    print('MPR: ', mpr)
